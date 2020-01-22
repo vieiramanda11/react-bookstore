b3a5d3217f5500e57b2c9bf4dc88f133da4ab4d1
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-22 18:35:39 -0300

    refactoring code

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -3,29 +3,22 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
 
-const BooksList = ({ books }) => {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Book ID</th>
-          <th>Title</th>
-          <th>Category</th>
-        </tr>
-      </thead>
-      <tbody>
-        { books.map((book) => <Book book={book} key={book.id} />)}
-      </tbody>
-    </table>
-  );
-};
+const BooksList = ({ books }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Book ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      { books.map((book) => <Book book={book} key={book.id} />)}
+    </tbody>
+  </table>
+);
 
-const mapStateToProps = (state) => {
-  console.log(state);
-  return {
-    books: state.books,
-  };
-};
+const mapStateToProps = (state) => ({ books: state.books });
 
 BooksList.defaultProps = {
   books: [],
