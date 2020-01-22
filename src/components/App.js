f86dd17121cf46cb04db4bf1b10ba40ed1445a7c
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-22 18:30:25 -0300

    creating form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import BooksList from '../containers/BooksList';
-// import BooksForm from '../containers/BooksForm';
+import BooksForm from '../containers/BooksForm';
 
 const App = () => (
   <div className="App">
     <BooksList />
+    <BooksForm />
   </div>
 );
 
