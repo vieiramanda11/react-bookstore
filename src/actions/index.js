156a8a2c6b4c7e58c81d3ef9aa722446714ce617
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-23 17:29:42 -0300

    creating filter reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,4 +2,4 @@ export const createBook = (book) => ({ type: 'CREATE_BOOK', payload: book });
 
 export const removeBook = (book) => ({ type: 'REMOVE_BOOK', payload: book });
 
-export const filter = (book) => ({ type: 'CHANGE_FILTER', payload: book});
+export const filter = (book) => ({ type: 'CHANGE_FILTER', payload: book });
