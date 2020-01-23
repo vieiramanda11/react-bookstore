c355001e2f3f4412b2f363666d22c4168f66f38b
vieiramanda11 <amanda_vieira@outlook.com>
2020-01-23 17:22:57 -0300

    creating filter action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
 export const createBook = (book) => ({ type: 'CREATE_BOOK', payload: book });
 
 export const removeBook = (book) => ({ type: 'REMOVE_BOOK', payload: book });
+
+export const filter = (book) => ({ type: 'CHANGE_FILTER', payload: book});
